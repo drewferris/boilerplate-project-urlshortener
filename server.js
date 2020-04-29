df3a2d408ea6015a732cbc7e13fb7cd80fdb6959
Drew Ferris <c_dferris@groupon.com>
2020-04-29 16:31:40 -0700

    challenge complete

--- server.js
+++ server.js
@@ -1,37 +1,86 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var crypto = require("crypto");
+var dns = require("dns");
+var cors = require("cors");
 
-var cors = require('cors');
+var Schema = mongoose.Schema;
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/** this project needs a db !! **/
+
+mongoose.connect(process.env.DB_URI);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: String,
+});
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+const Url = mongoose.model("Url", urlSchema);
+
+app.post("/api/shorturl/new", function (req, res) {
+  console.log(req.body);
+  const { body } = req;
+  const { url } = body;
+  const stub = url.split("://")[1];
+
+  dns.lookup(stub, (err, address, family) => {
+    if (err) {
+      return res.json({ error: "invalid URL" });
+    }
+
+    const hash = crypto.createHash("sha256");
+    hash.update(url);
+
+    const urlData = { original_url: url, short_url: hash.digest("hex") };
+    const newUrl = new Url(urlData);
+
+    newUrl.save(function (err, data) {
+      if (err) return console.error(err);
+    });
+
+    res.json(urlData);
+  });
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const { params } = req;
+  const { id } = params;
+
+  Url.find({ short_url: id }, (err, data) => {
+    if (err) {
+      res.json({err});
+    }
+    res.redirect(data[0].original_url);
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Node.js listening ...");
 });
